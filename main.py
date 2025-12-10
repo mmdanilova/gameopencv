1e92c2a3cb31e30c9fc058c57ccb149ef68c0474
Alex <minionalex24@gmail.com>
2025-12-10 15:02:43 +0300

    changes

--- main.py
+++ main.py
@@ -4,5 +4,4 @@ while True:
     ret, frame = cap.read()
     if cv2.waitKey(1) & 0xFF == ord('q') or not ret:
         break
-    cv2.imshow("Hands", frame)
-print("Y")
+    cv2.imshow("Водпровод", frame)
