2d0e768c46e5b1b115dd4744973177c8ff4a00f1
Alex <minionalex24@gmail.com>
2025-12-10 14:28:12 +0300

    camera

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-
+import cv2
+cap = cv2.VideoCapture(1)
+while True:
+    ret, frame = cap.read()
+    if cv2.waitKey(1) & 0xFF == ord('q') or not ret:
+        break
+    cv2.imshow("Hands", frame)
