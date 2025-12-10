9e9df0c80a551a3f285bd0f002e52a0ffd86c866
mmdanilova <mariadanbox@gmail.com>
2025-12-10 14:56:21 +0300

    program2 updated

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import cv2
-cap = cv2.VideoCapture(1)
+cap = cv2.VideoCapture(0)
 while True:
     ret, frame = cap.read()
     if cv2.waitKey(1) & 0xFF == ord('q') or not ret:
         break
     cv2.imshow("Hands", frame)
-print("Y")
